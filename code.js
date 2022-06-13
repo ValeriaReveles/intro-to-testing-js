4342ee020d388419c46b6e39c4a08f6683a35718
Valeria <valeriareveles12@gmail.com>
2022-06-13 15:27:35 -0500

    exercise 6 completed

--- code.js
+++ code.js
@@ -7,14 +7,19 @@ function helloWorld() {
 //     return "Hello, Jane!";
 // }
 
-function sayHello(input){
-    if (input === "Alex"){
-        return "Hello, Alex!";
-    }else if (input === "Pat"){
-        return "Hello, Pat!"
-    }else{
-        return "Hello, Jane!"
-    }
+// function sayHello(input){
+//     if (input === "Alex"){
+//         return "Hello, Alex!";
+//     }else if (input === "Pat"){
+//         return "Hello, Pat!"
+//     }else{
+//         return "Hello, Jane!"
+//     }
 }
 
+//Exercise 6 Refactored function:
+
+function sayHello(input){
+    return "Hello, " + input + "!";
+}
 
