3b0d6058bf8f09a4dac07d4f9a4abb18acf1b0f5
Valeria <valeriareveles12@gmail.com>
2022-06-13 15:12:50 -0500

    exercise 4 completed

--- tests.js
+++ tests.js
@@ -25,7 +25,16 @@ describe('sayHello', function(){
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+});
+
+describe('sayHello', function(){
+    it('should return a string when called.', function(){
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
 });
 
